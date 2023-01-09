b470bf4827d3c8fdc0309e69310398849d2c73bc
Samuel Pomeroy <1690@holbertonschool.com>
2023-01-09 19:49:11 +0000

    monoplanar adding shared KbdInputMgr functions

---
+++ monoplanar_demo/src/KbdInputMgr.cc
@@ -0,0 +1,31 @@
+#include "KbdInputMgr.hh"
+
+bool KbdInputMgr::keyDownThisFrame(const int32_t keysym) {
+    // remember that operator[] inserts a new key if not found
+    auto ks_it { key_states.find(keysym) };
+    if (ks_it != key_states.end())
+        return (ks_it->second.keyDownThisFrame());
+    return false;
+}
+
+bool KbdInputMgr::isPressed(const int32_t keysym) {
+    // remember that operator[] inserts a new key if not found
+    auto ks_it { key_states.find(keysym) };
+    if (ks_it != key_states.end())
+        return (ks_it->second.isPressed());
+    return false;
+}
+
+bool KbdInputMgr::isReleased(const int32_t keysym) {
+    // remember that operator[] inserts a new key if not found
+    auto ks_it { key_states.find(keysym) };
+    if (ks_it != key_states.end())
+        return (ks_it->second.isReleased());
+    return true;
+}
+
+void KbdInputMgr::decayToAutorepeat() {
+    for (auto& pair : key_states) {
+        pair.second.decayToAutorepeat();
+    }
+}

91f89981963d10d6428fe17cff65d8d2370f91ac
Samuel Pomeroy <1690@holbertonschool.com>
2023-01-21 00:02:04 +0000

    monoplanar main cleanup pass

--- monoplanar_demo/main.cc
+++ monoplanar_demo/main.cc
@@ -2,6 +2,7 @@
 #include "WindowMgr.hh"
 #include "SdlWindowMgr.hh"
 #include "Settings.hh"     // TtyDisplayMode
+#include "Xterm.hh"        // CtrlSeqs
 
 #include <getopt.h>        // option getopt_long optind
 #include <ctype.h>         // tolower
@@ -10,8 +11,8 @@
 #include <string>
 
 
-// static contexpr class members in C++11 require definition outside of the
-//   class, doing so here in main follows ODR
+// static contexpr class members in C++11 require declaration outside of the
+//   class, doing so once here follows ODR
 constexpr std::array<const char*, 10> WindowMgr::wall_tex_paths;
 
 constexpr char SdlWindowMgr::SKY_TEX_PATH[];
@@ -20,7 +21,12 @@ constexpr char SdlWindowMgr::FONT_PATH[];
 
 enum class IoMode  { Uninitialized, Tty, Sdl };
 
-static void printUsage(const char *exec_filename) {
+/**
+ * @brief generate formatted usage message
+ *
+ * @param exec_filename - executable file name
+ */
+static void printUsage(const char* exec_filename) {
     std::cerr << "\nUsage: " << exec_filename << "...\n\n" <<
         "\t-m=mapfile\n" <<
         "\t--map=mapfile\t Opens mapfile path to initialize maze map (required)\n" <<
@@ -39,17 +45,28 @@ static void printUsage(const char *exec_filename) {
         std::endl;
 }
 
-static int getOptions(int argc, char* const argv[],
+/**
+ * @brief parse command line options
+ *
+ * @param argc             - command token count
+ * @param argv             - array of command tokens (char* const* expected by
+ *                             getopt_long)
+ * @param map_filename     - set by reference to layout map file name
+ * @param io_mode          - enum set by reference to tty or sdl mode
+ * @param tty_display_mode - enum set by reference to tty pixel mode
+ *
+ * @return 0 on success, 1 on failure
+ */
+static int getOptions(const int argc, char* const argv[],
                       std::string& map_filename, IoMode& io_mode,
                       TtyDisplayMode& tty_display_mode) {
-    // colon after opt char signifies that it takes an arg
-    constexpr char optstring[] { "Xt::m:" };
     constexpr struct option long_options[] {
-        {"SDL",     no_argument,       nullptr,  'X' },
-        {"tty",     optional_argument, nullptr,  't' },
-        {"map",     required_argument, nullptr,  'm' },
+        {"SDL", no_argument,       nullptr, 'X' },
+        {"tty", optional_argument, nullptr, 't' },
+        {"map", required_argument, nullptr, 'm' },
         {nullptr, 0, 0, 0 }   // required sentinel with null name field
     };
+    constexpr char optstring[] { "Xt::m:" };
 
     int c;
     int option_i { 0 };
@@ -72,7 +89,8 @@ static int getOptions(int argc, char* const argv[],
             }
             io_mode = IoMode::Tty;
             // if `-t=`(arg), need to trim leading '='
-            char* trimmed_optarg { (optarg && optarg[0] == '=') ? optarg + 1 : optarg };
+            char* trimmed_optarg {
+                (optarg && optarg[0] == '=') ? optarg + 1 : optarg };
             // string from nullptr is undefined behavior
             std::string optarg_s { trimmed_optarg ? trimmed_optarg : "" };
             for (auto& c : optarg_s)
@@ -118,7 +136,15 @@ static int getOptions(int argc, char* const argv[],
     return 0;
 }
 
-int main(int argc, char* const argv[]) {
+/**
+ * @brief entry point
+ *
+ * @param argc - command token count
+ * @param argv - array of command tokens (char* const* expected by getOptions)
+ *
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(const int argc, char* const argv[]) {
     std::string map_filename;
     IoMode io_mode  { IoMode::Uninitialized };
     TtyDisplayMode tty_display_mode { TtyDisplayMode::Uninitialized };
@@ -133,8 +159,16 @@ int main(int argc, char* const argv[]) {
     if (io_mode == IoMode::Tty && tty_display_mode == TtyDisplayMode::Uninitialized)
         tty_display_mode = TtyDisplayMode::Ascii;
 
-    // TBD: wrap in try/except to do emergency cleanup on throwing
-    //   (eg ungrab keyboard device and ensure cursor is not hidden when in tty mode)
-    App app(argv[0], map_filename, (io_mode == IoMode::Tty), tty_display_mode);
-    app.run();
+    try {
+        App app(argv[0], map_filename, (io_mode == IoMode::Tty), tty_display_mode);
+        app.run();
+    } catch (const std::exception& e) {
+        // sudden failure of app in tty mode can leave cursor hidden or terminal
+        //   bg color set
+        if (io_mode == IoMode::Tty) {
+            std::cout << Xterm::CtrlSeqs::CharDefaults() <<
+                Xterm::CtrlSeqs::ShowCursor();
+        }
+        throw e;
+    }
 }

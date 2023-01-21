4c473fb582a7e0ea2c09a70c2271afc91ae3ef1b
Samuel Pomeroy <1690@holbertonschool.com>
2023-01-21 03:29:31 +0000

    monoplanar KeyState, Settings cleanup pass

--- monoplanar_demo/include/KeyState.hh
+++ monoplanar_demo/include/KeyState.hh
@@ -4,12 +4,11 @@
 #include <cstdint>              // int32_t
 
 
-// See linux/input.h for struct input_event
-// See SDL2/SDL_events.h for SDL_RELEASED and SDL_PRESSED
-enum KeyValue { Release,       // input_event.value release,    SDL_RELEASED
-                Press,         // input_event.value press,      SDL_PRESSED
-                Autorepeat };  // input_event.value autorepeat,
-                               //   (SDL uses SDL_KeyboardEvent.repeat as separate flag)
+// Release    : input_event.value = 0 : SDL_KeyboardEvent.state = SDL_RELEASED
+// Press      : input_event.value = 1 : SDL_KeyboardEvent.state = SDL_PRESSED
+// Autorepeat : input_event.value = 2 : SDL_KeyboardEvent.repeat = 1
+// (See linux/input.h for input_event; SDL2/SDL_events.h for SDL_KeyboardEvent)
+enum KeyValue { Release, Press, Autorepeat };
 
 class KeyState {
 protected:
@@ -17,43 +16,62 @@ protected:
     bool repeat  { false };  // key down event was in earlier frame
 
 public:
-    // input_event.code (__u16) or SDL_keycode (Sint32)
+    // keysym code, could be input_event.code (__u16) or SDL_keycode (Sint32)
     // (SDL_keycode enum range may not require a 4-byte int, but also uses
     //   bitmask SDLK_SCANCODE_MASK (1<<30), so we keep it 4 bytes)
     int32_t code;
 
-    // KeyState() only needed if map operator[] called with missing key,
-    //   which should not happen, so deleting it acts like an assert
+    // KeyState() only needed if map operator[] called with missing key
     KeyState() = delete;
     KeyState(const int32_t c) : code(c) {};
 
-    // input_event.value (__s32) or SDL_KeyboardEvent.state (Uint8)
-    // int size throttled to 1 byte due to only handling KeyValue values
-    // Not typing state as KeyValue to allow implicit conversion from __s32
+    /**
+     * @brief align key state with input
+     *
+     * @param state - new key state; could be input_event.value (__s32) or
+     *   SDL_KeyboardEvent.state (Uint8) (narrowed to 1 byte due to values
+     *   being 0-2, but uint8_t rather than KeyValue to allow implicit
+     *   conversion from __s32)
+     */
     void update(const uint8_t state) {
         if (state < KeyValue::Autorepeat) {
             repeat = false;
             pressed = static_cast<bool>(state);
         }
     }
-
+    /**
+     * @brief test if key was marked as pressed on this game frame
+     *
+     * @return true if pressed and not autorepreat
+     */
     inline bool keyDownThisFrame() const {
         return (pressed && !repeat);
     }
-
+    /**
+     * @brief test if key is currently pressed
+     *
+     * @return true if key is down
+     */
     inline bool isPressed() const {
         return (pressed);
     }
-
+    /**
+     * @brief test if key is currently not pressed
+     *
+     * @return true if key is not down
+     */
     inline bool isReleased() const {
         return (!pressed);
     }
-
-    // Game frames may pass between consuming a key press event and its first
-    //   autorepeat event; so to maintain the distinction between a key being
-    //   pressed and a key being held down at frame granularity, this can be
-    //   used to mark any keys pressed this frame as held (repeat) at the end
-    //   of that frame
+    /**
+     * @brief convert key press event from current game frame to autorepeat
+     *
+     * @notes Game frames may pass between consuming a key press event and its
+     *   first autorepeat event; so to maintain the distinction between a key
+     *   being pressed and a key being held down at frame granularity, this
+     *   can be used to mark any keys pressed this frame as held, or
+     *   repeating, at the end of that frame.
+     */
     inline void decayToAutorepeat() {
         if (pressed)
             repeat = true;
--- monoplanar_demo/include/Settings.hh
+++ monoplanar_demo/include/Settings.hh
@@ -4,7 +4,6 @@
 
 enum class TtyDisplayMode { Uninitialized, Ascii, ColorCode, TrueColor };
 
-// TBD: reset defaults after development
 struct Settings {
     TtyDisplayMode  tty_display_mode    { TtyDisplayMode::Uninitialized };
 
@@ -12,14 +11,12 @@ struct Settings {
     // minimap height : screen height
     double          map_proportion      { 1.0/3 };
 
-    bool            show_fps            { false };
+    bool            show_fps            { true };
 
-    bool            debug_mode          { true };
-
+    bool            debug_mode          { false };
     // when true, use real ray distance to wall rather than perpendicular
     //   camera plane distance
     bool            euclidean           { false };
-
     // used to determine player movement speed, as pegged to frame rate
     double          base_movement_rate  { 5.0 };
     // expressed as percentage of base_movement_rate

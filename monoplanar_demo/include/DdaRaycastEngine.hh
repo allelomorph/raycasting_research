5dbb544da72bccec37ac0e7a6cf550596e3d992a
Samuel Pomeroy <1690@holbertonschool.com>
2023-01-21 00:53:50 +0000

    monoplanar DdaRaycastEngine cleanup pass

--- monoplanar_demo/include/DdaRaycastEngine.hh
+++ monoplanar_demo/include/DdaRaycastEngine.hh
@@ -13,29 +13,24 @@
 enum class WallOrientation { NS, EW };
 
 struct FovRay {
-private:
+    // ray direction
+    Vector2d dir;
     struct WallHit {
-        double          dist { 0.0 };     // distance to first wall collision
-        WallOrientation algnmt;           // NS or EW alignment of wall hit
-        uint8_t         tex_key;          // layout.tile(map_x, map_y)
-        double          x;                // when viewing grid tile as wall unit
-                                          //   from player's perspective, x in
-                                          //   wall unit face where ray hit
-                                          //   (expressed as fraction of wall unit,
-                                          //   with 0.0 to player's left when
-                                          //   facing side hit)
-    };
-public:
-    Vector2d            dir;              // ray direction
-    WallHit             wall_hit;
+        // distance to first wall collision
+        double          dist { 0.0 };
+        // NS or EW alignment of wall hit
+        WallOrientation algnmt;
+        // layout.tile(map_x, map_y)
+        uint8_t         tex_key;
+        // when viewing a grid tile as a square wall unit from the player's
+        //   perspective, x in the wall unit face where ray hit (expressed as
+        //   fraction of wall unit, with 0.0 to player's left when facing wall)
+        double          x;
+    }        wall_hit;
 };
 
 class DdaRaycastEngine {
 private:
-    // TBD: better as member of Matrix?
-    // rotate Vector counterclockwise around origin
-    Vector2d rotateVector2d(const Vector2d& vec, const double radians);
-
     // used to set FOV from window aspect ratio to maintain square wall units
     //   in rendered view (calibrated from reference (627w:480h):0.666666 =
     //   1.959375, rounded up as calibration was by eye)
@@ -64,27 +59,82 @@ public:
     // When the player rotates, player_dir and view_plane should always remain
     //   perpendicular and of constant magnitude.
     Vector2d view_plane { 1, 0 };
+    // window horizontal pixel count
+    uint16_t window_w;
 
-    uint16_t screen_w;
     std::vector<FovRay> fov_rays;
 
     Layout layout;
 
+    /**
+     * @brief scale engine to window size
+     *
+     * @param tty_io - game currently in tty display mode
+     * @param w      - window width in pixels
+     * @param h      - window height in pixels
+     */
     void fitToWindow(const bool tty_io,
                      const uint16_t w, const uint16_t h);
 
+    /**
+     * @brief parse map file into stage layout
+     *
+     * @param map_filename - map file
+     */
     inline void loadMapFile(const std::string& map_filename) {
         layout.loadMapFile(map_filename, player_pos);
     }
 
-    void castRay(const uint16_t screen_x, const Settings& settings);
+    /**
+     * @brief apply DDA algorithm to cast ray from player position to first wall
+     *   hit
+     *
+     * @param window_x - horizontal window pixel coordinate
+     * @param settings - current game settings
+     */
+    void castRay(const uint16_t window_x, const Settings& settings);
+    /**
+     * @brief cast all rays in FOV
+     *
+     * @param settings - current game settings
+     */
     void castRays(const Settings& settings);
 
+    /**
+     * @brief rotate player counterclockwise
+     *
+     * @param rot_speed - player rotation speed, pegged to FPS
+     */
     void playerTurnLeft(const double rot_speed);
+    /**
+     * @brief rotate player clockwise
+     *
+     * @param rot_speed - player rotation speed, pegged to FPS
+     */
     void playerTurnRight(const double rot_speed);
+    /**
+     * @brief move player to left
+     *
+     * @param move_speed - travel speed across map, pegged to FPS
+     */
     void playerStrafeLeft(const double move_speed);
+    /**
+     * @brief move player to right
+     *
+     * @param move_speed - travel speed across map, pegged to FPS
+     */
     void playerStrafeRight(const double move_speed);
+    /**
+     * @brief move player forward
+     *
+     * @param move_speed - travel speed across map, pegged to FPS
+     */
     void playerMoveFwd(const double move_speed);
+    /**
+     * @brief move player backward
+     *
+     * @param move_speed - travel speed across map, pegged to FPS
+     */
     void playerMoveBack(const double move_speed);
 };
 
--- monoplanar_demo/src/DdaRaycastEngine.cc
+++ monoplanar_demo/src/DdaRaycastEngine.cc
@@ -6,8 +6,8 @@
 
 void DdaRaycastEngine::fitToWindow(const bool tty_io,
                                    const uint16_t w, const uint16_t h) {
-    screen_w = w;
-    fov_rays.resize(screen_w);
+    window_w = w;
+    fov_rays.resize(window_w);
 
     // widen FOV to match aspect ratio to always render square-looking wall units
     double curr_aspect_ratio { double(w) / h };
@@ -23,12 +23,12 @@ void DdaRaycastEngine::fitToWindow(const bool tty_io,
     view_plane.y *= target_vpm_to_curr_vpm_ratio;
 }
 
-void DdaRaycastEngine::castRay(const uint16_t screen_x,
+void DdaRaycastEngine::castRay(const uint16_t window_x,
                                const Settings& settings) {
     // x coordinate in the camera plane represented by the current
     //   screen x coordinate, calculated so that the left edge of the
     //   camera plane is -1.0, center 0.0, and right edge is 1.0
-    double camera_x { 2 * screen_x / double(screen_w) - 1 };
+    double camera_x { 2 * window_x / double(window_w) - 1 };
     FovRay ray;
 
     // ray origin is player_pos
@@ -78,7 +78,6 @@ void DdaRaycastEngine::castRay(const uint16_t screen_x,
 
     // perform DDA algo, or the incremental casting of the ray
     // moves to a new map unit square every loop, as directed by map_step values
-    // TBD: does orientation need to be set every loop?
     WallOrientation alignment;
     while (!layout.tileIsWall(map_x, map_y)) {
         if (dist_next_unit_x < dist_next_unit_y) {
@@ -90,7 +89,6 @@ void DdaRaycastEngine::castRay(const uint16_t screen_x,
             map_y += map_step_y;
             alignment = WallOrientation::EW;
         }
-        // TBD: what about OneLoneCoder's max cast distance?
     }
 
     ray.wall_hit.algnmt = alignment;
@@ -98,28 +96,19 @@ void DdaRaycastEngine::castRay(const uint16_t screen_x,
 
     // Calculate distance to wall hit from camera plane, moving
     //   perpendicular to the camera plane. If the actual length of the
-    //   ray cast from player to wall is used, one can observe a projection
-    //   that is more distorted as one approaches the wall.
-
-    // TBD: this paragarph is now conjecture: ", the expected result would be
-    //   a fisheye effect. For example, if the player were squarely
-    //   facing a wall (wall parallel to camera plane,) in the resulting
-    //   render the wall should appear of even height. If using the
-    //   length of the rays as they hit the wall, the wall would instead
-    //   appear to taper to the left and right ends of the display."
-    //
+    //   ray cast from player to wall is used, this creates a distorted
+    //   projection, which can be observed by setting the `euclidean` game
+    //   option.
     // One way to calculate the perpendicular camera plane distance would be
     //   to use the formula for shortest distance from a point to a line,
     //   where the point is where the wall was hit, and the line is the
     //   camera plane.
-    // TBD: evaluate and corroborate this last paragraph
-    // However, it can be done more simply: due to how dist_per_unit_? and
-    //   dist_next_unit_? were scaled by a factor of |ray.dir| above, the
+    // However, it can be done more simply: due to how dist_per_unit_* and
+    //   thus dist_next_unit_* are proportional to ray.dir, the ending
     //   length of dist_next_unit_? already almost equals the perpendicular
-    //   camera plane distance. We just need to subtract dist_per_unit_? once,
-    //   going one step back in the casting, as in the DDA loop above we went
-    //   one step further to end up inside the wall.
-
+    //   camera plane distance. It is only ncessary to subtract dist_per_unit_*
+    //   once, or go one step back in the casting, as the DDA loop above ends
+    //   once inside a wall.
     if (settings.euclidean) {  // actual ray distance from player_pos
         ray.wall_hit.dist = (ray.wall_hit.algnmt == WallOrientation::NS) ?
             dist_next_unit_x : dist_next_unit_y;
@@ -138,28 +127,25 @@ void DdaRaycastEngine::castRay(const uint16_t screen_x,
     // Expressed as fraction of 1 grid unit (0.0 on the left)
     ray.wall_hit.x -= std::floor(ray.wall_hit.x);
 
-    fov_rays[screen_x] = ray;
+    fov_rays[window_x] = ray;
 }
 
 void DdaRaycastEngine::castRays(const Settings& settings) {
-    for (uint16_t screen_x { 0 }; screen_x < screen_w; ++screen_x) {
-        castRay(screen_x, settings);
+    for (uint16_t window_x { 0 }; window_x < window_w; ++window_x) {
+        castRay(window_x, settings);
     }
 }
 
-// CCW
 void DdaRaycastEngine::playerTurnLeft(const double rot_speed) {
     player_dir.rotate(rot_speed);
     view_plane.rotate(rot_speed);
 }
 
-// CW
 void DdaRaycastEngine::playerTurnRight(const double rot_speed) {
     player_dir.rotate(-rot_speed);
     view_plane.rotate(-rot_speed);
 }
 
-// Vector2d is { x, y }
 void DdaRaycastEngine::playerStrafeLeft(const double move_speed) {
     double start_ppx { player_pos.x };
     if (!layout.tileIsWall(player_pos.x - player_dir.y * move_speed, player_pos.y))
@@ -176,7 +162,6 @@ void DdaRaycastEngine::playerStrafeRight(const double move_speed) {
         player_pos.y -= player_dir.x * move_speed;
 }
 
-// Vector2d is { x, y }
 void DdaRaycastEngine::playerMoveFwd(const double move_speed) {
     double start_ppx { player_pos.x };
     if (!layout.tileIsWall(player_pos.x + player_dir.x * move_speed, player_pos.y))

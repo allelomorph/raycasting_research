9ae159b40edd9de092d01774f2c190a6785c8091
Samuel Pomeroy <1690@holbertonschool.com>
2023-11-17 19:44:20 +0000

    small formatting change to set_strict_compile_options

--- monoplanar_demo/cmake/SetStrictCompileOptions.cmake
+++ monoplanar_demo/cmake/SetStrictCompileOptions.cmake
@@ -2,16 +2,19 @@
 macro(set_strict_compile_options target)
   if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     target_compile_options(${target} PRIVATE
-      /W4 /bigobj)
+      /W4 /bigobj
+      )
   endif()
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     target_compile_options(${target} PRIVATE
-      -Wall -Wextra -Wpedantic)
+      -Wall -Wextra -Wpedantic
+      )
   endif()
 
   if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     target_compile_options(${target} PRIVATE
-      -Wall -Wextra -Wpedantic -Werror)
+      -Wall -Wextra -Wpedantic -Werror
+      )
   endif()
 endmacro()

17a592e816066617d7f6c260cb8992aad6657b8a
Samuel Pomeroy <1690@holbertonschool.com>
2023-11-21 06:32:06 +0000

    renamed LocatePackageOrFetchContent to fetch_if_not_found

--- monoplanar_demo/cmake/FetchIfNotFound.cmake
+++ monoplanar_demo/cmake/FetchIfNotFound.cmake
@@ -1,26 +1,24 @@
 # newest features used: FetchContent v3.11, FetchContent_MakeAvailable v3.14
-# targeting cmake v3.16
-cmake_minimum_required(VERSION 3.14)
+cmake_minimum_required(VERSION 3.16)
 
-option(FETCH_ALL_DEPENDENCIES
-  "No attempt is made to find packages, instead all are handled with \
-FetchContent"
+option(SKIP_FIND_BEFORE_FETCH
+  "No attempt is made to find packages, instead all are handled with FetchContent"
   OFF)
 
 # find_package integration in FetchContent requires cmake 3.24:
 #   https://cmake.org/cmake/help/v3.27/guide/using-dependencies/index.html#fetchcontent-and-find-package-integration
-# LocatePackageOrFetchContent roughly emulates this behavior for lower versions
+# fetch_if_not_found roughly emulates this behavior for lower versions
 #   of cmake
-# !!! this relies on scripts used by find_package creating INTERFACE library
-#   targets with the same "namespaced" naming as ALIAS targets created by
-#   FetchContent
-macro(LocatePackageOrFetchContent
+# !!! This relies on scripts used by find_package creating INTERFACE library
+#   targets with the same names (including namespaces, if any,) as ALIAS targets
+#   created by FetchContent
+macro(fetch_if_not_found
     PKG_NAME    # expects string: package name
     FP_OPTIONS  # expects list:   find_package args beyond first
     FC_OPTIONS  # expects list:   FetchContent_Declare(ExternalProject_Add)
                 #                   args beyond first
                 )
-  if(NOT FETCH_ALL_DEPENDENCIES)
+  if(NOT SKIP_FIND_BEFORE_FETCH)
     find_package(${PKG_NAME} ${FP_OPTIONS})
   endif()
   if(NOT ${PKG_NAME}_FOUND)
@@ -28,6 +26,6 @@ macro(LocatePackageOrFetchContent
     FetchContent_Declare(${PKG_NAME} ${FC_OPTIONS})
     FetchContent_MakeAvailable(${PKG_NAME})
   endif()
-  # presence of package can now be tested by
-  # if(<pkg_name>_FOUND OR <lowercase pkg_name>_POPULATED)
+  # presence of package can now be tested by:
+  #   `if(<pkg_name>_FOUND OR <lowercase pkg_name>_POPULATED)`
 endmacro()
